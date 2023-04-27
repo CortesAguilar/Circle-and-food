facd211ca9d3ad88f66d478948a2d146d5787af8
Ulises Cortés <L22211541@tijuana.tecnm.mx>
2023-04-27 04:47:07 +0000

    Circle and food game

--- sketch.js
+++ sketch.js
@@ -1,102 +1,131 @@
-class Figura {
-   constructor(x, y, alto, ancho, vx, vy) {
-    this.posicion = createVector(x,y);
-    this.alto = alto;
-    this.ancho = ancho;
-    this.fillred = 255;
-    this.fillgreen = 87;
-    this.fillblue = 57;
-    this.velocidad = createVector(vx,vy);
-  }
-  update()
-  {
-      if (this.posicion.x + this.ancho >= 400)
-        {  
-          let valor = random(3); 
-          this.velocidad.x = this.velocidad.x * -valor;
-         this.velocidad.y = this.velocidad.y * -valor;
-        }
-      this.posicion.add(this.velocidad);
-  }
-  
-}
-
-class Rectangulo extends Figura{
-  constructor(x, y, alto, ancho, vx, vy) {
-      super(x, y, alto, ancho, vx, vy);
-  }
-  
-draw()
-  {
-fill(this.fillred,this.fillgreen,this.fillblue);
-rect(this.posicion.x,this.posicion.y,this.alto,this.ancho);
-  }
-}
-
-class Elipse extends Figura{
-  constructor(x, y, alto, ancho, vx, vy) {
-      super(x, y, alto, ancho, vx, vy);
-  }
-  
-draw()
-  {
-fill(this.fillred,this.fillgreen,this.fillblue);
-ellipse(this.posicion.x,this.posicion.y,this.alto,this.ancho);
-  }
-}
-
-var figuras = [];
-var dibujando = 'circulo';
-var btnCirculo = null;
-var btnRectangulo = null;
-
-
-function mouseClicked() {
-  // Se crea un objeto según la opción actual
-if (mouseY > 25)
-  {
-  if (dibujando == 'circulo')
-    figuras.push(new Elipse(mouseX,mouseY,20,20,3,1));
-  else if (dibujando == 'rectangulo')
-    figuras.push(new Rectangulo(mouseX,mouseY,20,20,2,1));
-  }
-
-  return false;
-}
+var gameObjects;
+var gameOver;
+var highScore = 0;
 
 function setup() {
-  createCanvas(400, 400);
-  
-  btnCirculo = createButton('Circulo');
-  btnCirculo.position(0, 0);
-  btnCirculo.mousePressed(changeCirculo);
-  btnCirculo.style( 'background-color','#cccccc');
-  
-  btnRectangulo = createButton('Rectangulo');
-  btnRectangulo.position(75, 0);
-  btnRectangulo.mousePressed(changeRectangulo);
+  createCanvas(800, 800);
+  gameOver = false;
+  gameObjects = [];
+  gameObjects.push(new Circle(createVector(width / 2, height / 2)));
+  gameObjects.push(new Food(createVector(100, 100)));
 }
 
-function changeCirculo()
-   {
-     btnCirculo.style( 'background-color','#cccccc');
-     btnRectangulo.style( 'background-color','#f0f0f0');
-     dibujando = 'circulo';
-   }
-function changeRectangulo()
-   {
-     btnRectangulo.style( 'background-color','#cccccc');
-     btnCirculo.style( 'background-color','#f0f0f0');
-     dibujando = 'rectangulo';
-   }
-
- 
-
 function draw() {
+  if (gameOver) {
+    calculateHighScore();
+    gameOverScreen();
+    if (mouseIsPressed) setup();
+  } else {
+    gameScreen();
+    playGame();
+  }
+}
+
+function calculateHighScore() {
+  if (gameObjects[0].ballsEaten > highScore)
+    highScore = gameObjects[0].ballsEaten;
+}
+
+function gameOverScreen() {
+  background("black");
+  fill(0, 102, 153);
+  textSize(32);
+  text("You just lost the easiest game ever", width / 6, height / 2);
+  text("Your score: " + gameObjects[0].ballsEaten, width / 6, height * 0.9);
+  text("High score: " + highScore, width * 0.55, height * 0.9);
+  text("Click anywhere to retry", width * 0.27, height * 0.55);
+}
+
+function gameScreen() {
   background(220);
-  figuras.forEach((fig) => 
-   {
-    fig.draw();
-    fig.update();
-   });
+  textSize(20);
+  fill("black");
+  text("Score : " + gameObjects[0].ballsEaten, width * 0.75, height * 0.1);
+}
+
+function playGame() {
+  gameObjects.forEach(function (value, index) {
+    value.display();
+    if (index == 0) value.update(gameObjects[1]);
+    else value.update(gameObjects[0]);
+  });
+}
+
+class GameObject {
+  constructor(position) {
+    if (this.constructor === GameObject)
+      throw new TypeError("Cannot create GameObject objects directly");
+    this.position = position;
+  }
+
+  display() {
+    fill(this.colour);
+    circle(this.position.x, this.position.y, this.size);
+  }
+
+  isInColission(gameObject) {
+    let distance = dist(
+      this.position.x,
+      this.position.y,
+      gameObject.position.x,
+      gameObject.position.y
+    );
+    let margin;
+
+    if (gameObject instanceof Food) margin = this.size / 2;
+    else margin = gameObject.size / 2;
+
+    if (distance <= margin) return true;
+
+    return false;
+  }
+}
+
+class Circle extends GameObject {
+  constructor(position) {
+    super(position);
+    this.colour = "green";
+    this.size = 20;
+    this.ballsEaten = 0;
+  }
+
+  update(foodObject) {
+    this.position = createVector(mouseX, mouseY);
+
+    if (this.isInColission(foodObject)) this.grow();
+
+    if (this.isOutOfScreen()) gameOver = true;
+  }
+
+  grow() {
+    this.size += 5;
+    this.ballsEaten++;
+  }
+
+  isOutOfScreen() {
+    if (
+      this.position.x + this.size / 2 >= width ||
+      this.position.x - this.size / 2 <= 0 ||
+      this.position.y + this.size / 2 >= height ||
+      this.position.y - this.size / 2 <= 0
+    )
+      return true;
+    return false;
+  }
+}
+
+class Food extends GameObject {
+  constructor(position) {
+    super(position);
+    this.colour = "white";
+    this.size = 10;
+  }
+
+  update(circleObject) {
+    if (this.isInColission(circleObject))
+      this.position = createVector(
+        random(10, width - this.size),
+        random(10, height - this.size)
+      );
+  }
 }
